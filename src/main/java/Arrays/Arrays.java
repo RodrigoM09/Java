a3403fb9c1bb7015c837e4859ce6e3658612831a
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-03-28 12:42:54 -0500

    autosave script

---
+++ src/main/java/Arrays/Arrays.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        // Declare an array of integers
+        int[] anArray;
+
+        // Allocate memory for 10 integers
+        anArray = new int[10];
+
+
+    }
+}
--- src/main/java/ExplicitDataTypeConversion.java
+++ src/main/java/ExplicitDataTypeConversion.java
@@ -4,6 +4,7 @@ public class ExplicitDataTypeConversion {
         double a = 50.50;
         System.out.println("double: " + a);
 
+        //Type casting, is a way to convert one data type to another.
         float f = (float) a;
         System.out.println("float: " + f);
 
