44d0fef34e841c5c7950a0c2144c760e59c704e2
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-03-28 13:23:27 -0500

    autosave script

---
+++ src/main/java/BitWiseOperator.java
@@ -0,0 +1,14 @@
+public class BitWiseOperator {
+    public static void main(String[] args){
+        // Initial values
+        int a = 5;
+        int b = 7;
+
+        // Bitwise AND
+        // 0101 & 0111=0101
+        System.out.println("a & b = " + (a & b));
+        // Bitwise OR
+        // 0101 | 0111=0111
+        System.out.println("a | b = " + (a | b));
+    }
+}
